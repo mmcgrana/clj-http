4d86724756dee8e59640439be111c415fb1f44f9
Matt Revelle <mrevelle@gmail.com>
2011-02-13 23:06:49 -0800

    Updated dep.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A Clojure HTTP library wrapping the Apache HttpComponents client."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.apache.httpcomponents/httpclient "4.0.3"]
+                 [org.apache.httpcomponents/httpclient "4.1"]
                  [commons-codec "1.4"]
                  [commons-io "1.4"]]
   :dev-dependencies [[swank-clojure "1.2.0"]
